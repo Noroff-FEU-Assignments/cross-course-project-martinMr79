8aab8dabac967477c2166ffda111ac40cb1a8f0c
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-03-23 00:02:22 +0100

    22.03

--- js/navbar.js
+++ js/navbar.js
@@ -5,12 +5,12 @@ const closeIcon = document.querySelector(".close-icon");
 const menuIcon = document.querySelector(".menu-icon");
 
 function toggle() {
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu"); 
+    if (menu.classList.contains("show-menu")) {
+        menu.classList.remove("show-menu"); 
         closeIcon.style.display = "none"; 
         menuIcon.style.display = "block"; 
     } else {
-        menu.classList.add("showMenu"); 
+        menu.classList.add("show-menu"); 
         closeIcon.style.display = "block"; 
         menuIcon.style.display = "none"; 
 
