dc78d1570ef1c84cd60a9dd158908e1b1c113e33
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-03-24 13:18:00 +0100

    working on add to cart

---
+++ js/add_to_cart.js
@@ -0,0 +1,3 @@
+var removeCartItem = document.getElementsByClassName("btn-danger")
+
+
---
+++ js/productsList.js
@@ -0,0 +1,49 @@
+const productArray = [
+    {
+        name: "Premium Raincoat",
+        gender: "male",
+        price: 795, 
+        offer: true,
+        id: 1
+    },
+
+    {
+        name: "Wind and water repellent",
+        gender: "male",
+        price: 995, 
+        offer: false,
+        id: 2
+    },
+
+    {
+        name: "Lightweight hiking",
+        gender: "male",
+        price: 1295, 
+        offer: false,
+        id: 3
+    },
+
+    {
+        name: "100% waterproof",
+        gender: "female",
+        price: 795, 
+        offer: true,
+        id: 4
+    },
+
+    {
+        name: "Ultra Warm",
+        gender: "female",
+        price: 1295, 
+        offer: true,
+        id: 5
+    }
+
+]
+
+
+
+
+
+/*inspired by*/
+/*https://m.youtube.com/watch?v=dYRgB-0xvdw*/
