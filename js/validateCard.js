905749b36df42c291c43280d058dfdc7174b8e46
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-03-26 20:33:22 +0100

    styling, and working on creditcard validation

---
+++ js/validateCard.js
@@ -0,0 +1,75 @@
+const form = document.querySelector("#contactForm");
+const nameInput = document.querySelector("#name");
+const nameError = document.querySelector("#nameError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const adressError = document.querySelector("#adressError");
+const validForm = document.querySelector(".validForm");
+const message = document.querySelector(".message");
+const button = document.querySelector("button");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (nameInput.value.trim().length > 0) {
+    nameError.style.display = "none";
+  } else {
+    nameError.style.display = "block";
+  }
+
+  if (subject.value.trim().length > 9) {
+    subjectError.style.display = "none";
+  } else {
+    subjectError.style.display = "block";
+  }
+
+  if (validateEmail(email.value) === true && email.value.trim().length > 5) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  console.log("hekk");
+}
+
+function checkIfButtonIsDisabled() {
+  if (
+    checkLength(nameInput.value, 1) &&
+    checkLength(subject.value, 10) &&
+    validateEmail(email.value, 5)
+  ) {
+    button.disabled = false;
+  } else {
+    message.innerHTML = "";
+    button.disabled = true;
+  }
+}
+
+nameInput.addEventListener("keyup", checkIfButtonIsDisabled);
+subject.addEventListener("keyup", checkIfButtonIsDisabled);
+
+function validateEmail(email) {
+  const regEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+  const matchPattern = regEx.test(email);
+  return matchPattern;
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  console.log(event.preventDefault());
+  message.innerHTML = '<div class="message">form passed successfully</div>';
+  form.reset();
+}
+
+form.addEventListener("submit", validateForm);
+form.addEventListener("submit", submitForm);
+
+function checkLength(value, len) {
+  if (value.trim().length >= len) {
+    return true;
+  } else {
+    return false;
+  }
+}
