8093b9890c62ddc4827cf598004262fa9aeca495
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-04-20 23:14:16 +0200

    img fixed

--- js/wp-api.js
+++ js/wp-api.js
@@ -6,13 +6,30 @@ async function getProducts(url) {
   const products = await response.json();
   console.log(products);
 
+
   products.forEach(function (product) {
     productContainer.innerHTML += `
-  <div class="product">
-  <h2>${product.name}</h2>
-  </div>
+    <div class="products">
+    <a href="Product_specific.html">
+
+
+  <img
+      src="${product.images[0].src}"
+      alt="${product.name}"
+      class="products-img"
+  </img>
+
+  </a>
+        <h3>${product.name}</h3>
+        <div class="product_description">
+        <p class="product-price">${product.price},-</p>
+        <button class="cta_small" data-product=${product.id}>Add to cart</button>
+        </div>
+    </div>
    `;
   });
 }
 
 getProducts(baseUrl);
+
+
