917eb1cd9f763e336d932f78bf2399bb7c1ca641
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-04-24 23:22:46 +0200

    finnal

--- js/index.js
+++ js/index.js
@@ -1,37 +1,43 @@
-const baseUrl = "https://www.volumvekt.no/rainydays/wp-json/wc/store/products?per_page=1";
+const baseUrl = "https://www.volumvekt.no/rainydays/wp-json/wc/store/products";
 const productContainer = document.querySelector(".products")
+const searchButton = document.querySelector(".search-button")
+
+
 
 async function getProducts(url){
     const response = await fetch(url);
     const products = await response.json();
     console.log(products)
 
+
+    
     products.forEach(function (product) {
         productContainer.innerHTML += `
-        <div class="products">
-        <a href="Product_specific.html?id=${product.id}">
-    
-    
-      <img
-          src="${product.images[0].src}"
-          alt="${product.name}"
-          class="products-img"
-      </img>
-    
-      </a>
-            <h3>${product.name}</h3>
-            <div class="product_description ">
-            <p>${product.price},-</p>
-            
-            <a class="cta_small" data-product=${product.id}>Add to cart</a>
-            </div>
-            </div>
+         <div class="products">
+    <a href="Product_specific.html?id=${product.id}">
+
+   <div class=also_purchased_container purchase> 
+  <img
+      src="${product.images[0].src}"
+      alt="${product.name}"
+      class="purchase"
+  </img>
+  </div>
+
+  </a>
+
         </div>
+    </div>
        `;
       });
+
+    }
+
+getProducts(baseUrl);
+
+searchButton.onclick = function(){
+    const searchInput = document.querySelector("#search-input").value;
+    const newUrl = baseUrl + `?search=${searchInput}`;
+    productContainer.innerHTML = ""; 
+    getProducts(newUrl);
 }
-
-
-
-
-getProducts(baseUrl)
